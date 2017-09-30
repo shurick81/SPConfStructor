89157e0990310ec15d2da2aa32f00b98682ae4f1
Alex <alexander@sapozhkov.net>
2017-09-30 13:09:29 +0300

    updating admin modules installing

--- SharePointOnPrem/installDSCResourcesAdmin.ps1
+++ SharePointOnPrem/installDSCResourcesAdmin.ps1
@@ -1,5 +1,11 @@
 #you need this for compiling mofs on your development machine
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+Install-Module -Name AzureRm.Resources -RequiredVersion 4.3.1 -Force
+Install-Module -Name AzureRm.Network -RequiredVersion 4.3.1 -Force
+Install-Module -Name AzureRM.Storage -RequiredVersion 3.3.1 -Force
+Install-Module -Name AzureRM.Compute -RequiredVersion 3.3.1 -Force
+
 Install-Module -Name xRemoteDesktopAdmin -Force
 Install-Module -Name xPSDesiredStateConfiguration -Force
 Install-Module -Name xActiveDirectory -Force
